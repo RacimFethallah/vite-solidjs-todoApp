8e2179bf7aab034bb25cfd05874fb4b065575a82
racimfethallah <ricdev.io@gmail.com>
2024-07-16 19:57:58 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import { createStore } from "solid-js/store";
+import "./App.css";
+import { batch, createSignal, For } from "solid-js";
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+function App() {
+  const [title, setTitle] = createSignal<string>("");
+  const [todos, setTodos] = createStore<Todo[]>([]);
+
+  const addTodo = (e: SubmitEvent) => {
+    e.preventDefault();
+    batch(() => {
+      setTodos([
+        { id: todos.length + 1, title: title(), completed: false },
+        ...todos,
+      ]);
+      setTitle("");
+    });
+  };
+
+  return (
+    <main class="flex flex-col items-center justify-center p-20">
+      <div class=" max-w-xl mx-auto my-8 p-8 bg-white rounded-lg shadow">
+        <h1 class="text-3xl font-bold mb-4 text-center text-black">
+          Svelte Todo App
+        </h1>
+
+        <form class="my-4" onSubmit={addTodo}>
+          <input
+            value={title()}
+            onInput={(e) => setTitle(e.currentTarget.value)}
+            type="text"
+            placeholder="Add a new todo"
+            class="p-2 border rounded mr-2"
+          />
+          <button class="bg-blue-500 text-white p-2 rounded">Add Todo</button>
+        </form>
+
+        <For each={todos}>
+          {(todo) => (
+            <div class="flex items-center justify-between p-2 border-b">
+              <div class="flex items-center">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() =>
+                    setTodos(
+                      todos.map((t) =>
+                        t.id === todo.id ? { ...t, completed: !t.completed } : t
+                      )
+                    )
+                  }
+                  class="mr-2"
+                />
+                <span
+                  class="text-black"
+                  classList={{ "line-through": todo.completed }}
+                >
+                  {todo.title}
+                </span>
+              </div>
+              <button
+                onClick={() => setTodos(todos.filter((t) => t.id !== todo.id))}
+                class="text-red-500"
+              >
+                Delete
+              </button>
+            </div>
+          )}
+        </For>
+      </div>
+    </main>
+  );
+}
+
+export default App;
